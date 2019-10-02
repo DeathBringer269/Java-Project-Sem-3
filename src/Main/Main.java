5d7d90e83a4db4f2a3514181819ae6d85f3b1a2a
Alan Dsilva <deathbringer2604@gmail.com>
2019-10-02 12:11:02 +0530

    intialized
    Created folders for fxml files its controllers and for css files
    Added start method to Main.java

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author Grace
+ */
+public class Main extends Application {
+    
+    public void start(Stage primaryStage) {
+        
+    }
+}
