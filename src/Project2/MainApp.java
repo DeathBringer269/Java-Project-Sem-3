1539c2883b2d4be9de24238571518071cbd8d19d
Alan Dsilva <deathbringer2604@gmail.com>
2019-10-04 21:11:11 +0530

    Created Project2 folder

---
+++ src/Project2/MainApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Grace
+ */
+public class MainApp extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+    }
+    
+    public static void main(String args[]) {
+        launch(args);
+    }
+}
