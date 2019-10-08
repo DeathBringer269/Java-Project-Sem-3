efa98a85f027d5924571b93c689f1e12f96b0463
Alan Dsilva <alandsilva2001@gmail.com>
2019-10-08 12:21:40 +0530

    sql tables

--- Sql/StudentLoginData.sql
+++ Sql/StudentLoginData.sql
@@ -1,9 +1,17 @@
 CREATE TABLE IF NOT EXISTS StudentLoginData (
-    `ID` INT,
-    `Name` VARCHAR(12) CHARACTER SET utf8,
-    `Username` INT,
-    `Password` INT
+    `ID` NUMERIC(3, 1),
+    `Name` VARCHAR(15) CHARACTER SET utf8,
+    `Username` NUMERIC(7, 1),
+    `Password` NUMERIC(7, 1)
 );
 INSERT INTO StudentLoginData VALUES
-    (1,'Alan Dsilva',182028,182028),
-    (2,'Sujoy Dchuna',182023,182023);
+    (1.0,'Alan Dsilva',182028.0,182028.0),
+    (2.0,'Sujoy Dchuna',182023.0,182023.0),
+    (3.0,'Priyen Dsouza',182032.0,182032.0),
+    (4.0,'Rebecca Dias ',182022.0,2525.0),
+    (5.0,'Nimisha George ',182036.0,182036.0),
+    (6.0,'Mark Coutinho',182004.0,182004.0),
+    (7.0,'Student 2',182042.0,182042.0),
+    (8.0,'Student 3',182043.0,182043.0),
+    (9.0,'Student 4',182044.0,182044.0),
+    (10.0,'Student 5',182055.0,182055.0);
